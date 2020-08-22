146b11075a481f403a66207514b02ef10243bf36
alyssaazhang <69354557+alyssaazhang@users.noreply.github.com>
2020-08-22 09:47:04 -0400

    basic layout

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -7,7 +7,71 @@ class Home extends Component {
 		return (
             <div className="container-fluid">
                 <h1>HOME</h1>
+						<div className="home">
+						<h2> some creative catchphrase </h2>
+						<p>
+						// some catchy text with a basic explanation of what it does
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+						culpa qui officia deserunt mollit anim id est laborum.
+						</p>
             </div>
+						<div className="inspiration">
+						// what inspired the idea
+						<h3> Inspiration </h3>
+						<p>
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+						accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+						quae ab illo inventore veritatis et quasi architecto beatae vitae
+						dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+						aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+						eos qui ratione voluptatem sequi nesciunt.
+						</p>
+						</div>
+						<div className="about">
+						<h3> How it works </h3>
+						<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+						</p>
+						<h3> Challenges we ran into </h3>
+						<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+						</p>
+						<h3> Accomplishments that we're proud of </h3>
+						<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+						</p>
+						<h3> What we learned </h3>
+						<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+						</p>
+						<h3> What's next for SongSmith </h3>
+						<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+						</p>
+						</div>
+						</div>
+
+
         )
 	}
 }
