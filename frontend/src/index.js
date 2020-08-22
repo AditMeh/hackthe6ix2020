073b5af410e403290625aeb41c30787456dbb74f
dora-su <dorasu02@gmail.com>
2020-08-22 19:10:08 -0400

    new landing page template

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
-import './index.css';
-import './components/audioplayer/styles.scss'
-import * as serviceWorker from './serviceWorker';
- 
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
 import App from './App';
-// import Firebase, { FirebaseContext } from './components/firebase';
- 
+import * as serviceWorker from './serviceWorker';
+
+//import './App.css';
+import './assets/scss/style.scss';
+
+const history = createBrowserHistory();
+
 ReactDOM.render(
-  <React.StrictMode>
-
-  {/* //<FirebaseContext.Provider value={new Firebase()}> */}
+  <Router history={history}>
     <App />
-  {/* //</FirebaseContext.Provider> */}
-  </React.StrictMode>,
-
-  document.getElementById('root'),
+  </Router>,
+  document.getElementById('root')
 );
- 
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
