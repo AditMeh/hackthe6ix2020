9d2d6544a0a881c653166b41cd7c37f38cb502d3
alyssaazhang <69354557+alyssaazhang@users.noreply.github.com>
2020-08-22 16:37:21 -0400

    fix scrolling and add navbar

---
+++ frontend/src/components/nav/nav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './nav.css';
+import {Link} from 'react-router-dom';
+
+function Nav() {
+  return (
+    <nav class="navbar navbar-expand-md ">
+      <Link to= '/'>
+        <h3 className="logo">SongSmith</h3>
+      </Link>
+      <div class="collapse navbar-collapse" id="nav-main">
+        <ul className="nav-links navbar-nav ml-auto" >
+          <Link to="/">
+            <li>Inspiration</li>
+         </Link>
+         <Link to="/">
+            <li>About</li>
+         </Link>
+         <Link to="/">
+            <li>Try It!</li>
+         </Link>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
