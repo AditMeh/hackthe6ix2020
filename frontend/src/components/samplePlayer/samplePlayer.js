2e6ab04d7d01ed9a52f2b9c09a36db2cd820710f
Oren Leung <oren.leung@gmail.com>
2020-08-22 15:13:21 -0400

    useless, since we not doing midi anymore

--- frontend/src/components/samplePlayer/samplePlayer.js
+++ frontend/src/components/samplePlayer/samplePlayer.js
@@ -8,11 +8,19 @@ const storage = firebaseApp.storage()
 class SamplePlayer extends Component {
     constructor(props) {
         super(props);
+        this.state = { value: '' };
+        this.handleChange = this.handleChange.bind(this);
         this.play = this.play.bind(this)
     }
 
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
     async play() {
-        var s = storage.refFromURL('gs://songsmith-98875.appspot.com/cs1-1pre.mid')
+        let fileName = this.state.value
+        const s = storage.refFromURL(`gs://songsmith-98875.appspot.com/${fileName}`)
         const url = await s.getDownloadURL()
         MIDIjs.play(url)
     }
@@ -20,7 +28,9 @@ class SamplePlayer extends Component {
     render() {
         return (
             <div>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
                 <button onClick={this.play}>play</button>
+                <p>try cs1-1pre.mid</p>
             </div>
         )
     }
